583e6176d5f85f6c4b637af5d9122a770e569f1c
bobrovice <bobrovice@gmail.com>
2018-02-23 13:28:39 +0300

    sorter solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = []
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element)
   }
 
   at(index) {
-    // your implementation
+    return this.array[index]
   }
 
   get length() {
-    // your implementation
+    return this.array.length
   }
 
   toArray() {
-    // your implementation
+    return this.array
   }
 
   sort(indices) {
-    // your implementation
-  }
+    let newArr = indices.sort().map(i => this.array[i]).sort(this.comp || ((left, right) => left - right))
+    indices.forEach((i, indices) => this.array[i] = newArr[indices])
+}
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction
   }
 }
 
